94d849d8854862769e68ffcd0b600b147a4490ec
Alexander Haase <ahaase@alexhaase.de>
2021-03-07 02:21:07 +0100

    Add abstract provider class
    The abstract provider class defines the interface, all providers should
    implement.

---
+++ src/zettel/provider/abstractProvider.py
@@ -0,0 +1,39 @@
+# This file is part of Zettel.
+#
+# Copyright (c) Alexander Haase <ahaase@alexhaase.de>
+#
+# This project is licensed under the MIT License. For the full copyright and
+# license information, please view the LICENSE file that was distributed with
+# this source code.
+
+import abc
+from typing import List
+
+from ..item import Item
+
+
+class AbstractProvider(abc.ABC):
+    """
+    Abstract :py:class:`.Item` provider.
+
+    This abstract class defines the interface, a provider needs to define. As a
+    provider basically is just a single method to be called, these classes won't
+    be complicated in design, but require a standard API to gather all items.
+    """
+
+    @abc.abstractmethod
+    def fetch(self) -> List[Item]:
+        """
+        Fetch all items from the provider's backend.
+
+        This method will be called to gather all items from the provider's
+        backend, which should be returned as list for further processing the
+        data in Zettel.
+
+        .. warning:: Providers should not alter the origin data in their related
+            backends to give reproducible results on subsequent calls.
+
+
+        :returns: A list of items this backend currently provides.
+        """
+        pass
